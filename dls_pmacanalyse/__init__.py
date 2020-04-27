1777af49e0afc3039cc0e813b84f24f67b795ad9
giles <gilesknap@gmail.com>
2020-04-27 07:35:09 +0100

    update to FLD python3 including black formatting

--- dls_pmacanalyse/__init__.py
+++ dls_pmacanalyse/__init__.py
@@ -1,16 +1,4 @@
 import os
 import sys
-from dls_pmacanalyse.dls_pmacanalyse import (
-    GlobalConfig, WebPage, ConfigError, Pmac
-)
 
-
-def load_version():
-    '''Load version from file and set as __version__ attribute.'''
-    package_dir = os.path.dirname(os.path.realpath(__file__))
-    filename = os.path.join(package_dir, 'VERSION')
-    with open(filename) as f:
-        return f.read().strip()
-
-
-__version__ = load_version()
+from dls_pmacanalyse.dls_pmacanalyse import ConfigError, GlobalConfig, Pmac, WebPage
---
+++ tests/test_test_python.py
@@ -0,0 +1,8 @@
+from dls_pmacanalyse.dls_pmacanalyse import PmacToken
+
+# todo need some tests for this module
+
+
+def test_hello_class_formats_greeting() -> None:
+    inst = PmacToken()
+    print(inst)
